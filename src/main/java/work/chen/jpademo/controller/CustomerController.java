3d3a57d41475aba25c63bf524b7a363b2f2f1137
chenlibrary <842120056@qq.com>
2021-05-10 15:38:39 +0800

    fix(CustomerController)  注释补充

--- src/main/java/work/chen/jpademo/controller/CustomerController.java
+++ src/main/java/work/chen/jpademo/controller/CustomerController.java
@@ -2,14 +2,19 @@ package work.chen.jpademo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import work.chen.jpademo.bean.DataCustomerListRequest;
+import work.chen.jpademo.bean.DataCustomerListResponse;
 import work.chen.jpademo.entity.CustomerEntity;
 import work.chen.jpademo.service.CustomerService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ProjectName: jpa-demo
@@ -29,18 +34,17 @@ public class CustomerController {
   @Autowired
   private CustomerService customerService;
 
-
   /**
    * Hibernate: insert into jpa_customer (age, name, phone, sex, id) values (?, ?, ?, ?, ?)
    * Hibernate: insert into jap_order (tprice, oid) values (?, ?)
    * Hibernate: update jap_order set cid=? where oid=?
+   * 主表（jpa_customer）数据及从表数据（jpa_order）数据保存
    * @return
    */
-  @RequestMapping(value = "/saveTest", method = RequestMethod.POST)
-  public int saveTest() {
+  @RequestMapping(value = "/saveCustomerAndOrder", method = RequestMethod.POST)
+  public int saveCustomerAndOrder() {
     return customerService.saveCustomerAndOrder();
   }
-
 
   /**
    * Hibernate: select customeren0_.id as id1_2_, customeren0_.age as age2_2_, customeren0_.name as name3_2_, customeren0_.phone as phone4_2_, customeren0_.sex as sex5_2_ from jpa_customer customeren0_
@@ -79,8 +83,6 @@ public class CustomerController {
 
   /**
    * left join 查询
-   * Hibernate: select customeren0_.id as id1_2_, customeren0_.age as age2_2_, customeren0_.name as name3_2_, customeren0_.phone as phone4_2_, customeren0_.sex as sex5_2_ from jpa_customer customeren0_ left outer join jap_order orderentit1_ on customeren0_.id=orderentit1_.assid where orderentit1_.oid=2
-   * Hibernate: select orderentit0_.assid as assid3_0_0_, orderentit0_.oid as oid1_0_0_, orderentit0_.oid as oid1_0_1_, orderentit0_.tprice as tprice2_0_1_ from jap_order orderentit0_ where orderentit0_.assid=?
    * @return
    */
   @RequestMapping(value = "/leftJoin", method = RequestMethod.POST)
--- src/main/java/work/chen/jpademo/service/LibraryService.java
+++ src/main/java/work/chen/jpademo/service/LibraryService.java
@@ -7,12 +7,28 @@ import java.util.List;
 
 public interface LibraryService {
 
+	/**
+	 * 模拟主表(jpa_library)及从表(jpa_booke)数据保存
+	 * @return
+	 */
 	LibraryEntity save();
 
+	/**
+	 *  模拟主表(jpa_library)数据更新
+	 * @return
+	 */
 	LibraryEntity update();
 
+	/**
+	 * 查找主表(jpa_library)所有的数据
+	 * @return
+	 */
 	List<LibraryEntity> findAll();
 
+	/**
+	 * 查找主表(jpa_library)及对应从表(jpa_booke)数据
+	 * @return
+	 */
 	List<DataLibraryListResponse> getLibraryAndBookData();
 
 }
