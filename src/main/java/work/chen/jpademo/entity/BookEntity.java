eb99c12517dd1dc462ee6fa14ed9ccab7770d05e
chenlibrary <842120056@qq.com>
2021-05-10 11:36:07 +0800

    fix(entity) 解释说明

--- src/main/java/work/chen/jpademo/entity/BookEntity.java
+++ src/main/java/work/chen/jpademo/entity/BookEntity.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-//@Data
 @Entity
 @Setter
 @Getter
@@ -43,6 +42,17 @@ public class BookEntity {
 	@Column(length = 1000)
 	private String introduce;
 
+	/**
+	 * @ManyToOne 多对一（声明在的多的一方）
+	 *		targetEntity : 目标实体类Class对象
+	 * @JoinColumn	关联关系
+	 * 		name :  关联字段名称（从表中关联字段的名称）
+	 *		referencedColumnName : 关联关系参照主表中的那个字段（这里是jpa_library表中的主键lid）
+	 *		foreignKey : 外键
+	 *			@ForeignKey(value = ConstraintMode.NO_CONSTRAINT) 放弃外键维护
+	 * @JsonIgnore ： Json压缩时忽略此属性（此属性压缩时不做处理）当使用Springboot的@RestController注解时，一定要追加
+	 * private LibraryEntity libraryEntity 一的一方 类与类之间的绑定
+	 */
 	@ManyToOne(targetEntity = LibraryEntity.class)
 	@JoinColumn(name = "assid", referencedColumnName = "lid", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
 	@JsonIgnore
--- src/main/java/work/chen/jpademo/entity/LibraryEntity.java
+++ src/main/java/work/chen/jpademo/entity/LibraryEntity.java
@@ -9,6 +9,13 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * LibraryEntity 主表类（一的一方）
+ * @Setter
+ * @Getter
+ *	注意：这里不可以使用lombok的@Data注解 （双向绑定后重新toString查询时会导致内存溢出）
+ */
+
 @Setter
 @Getter
 @Entity
@@ -43,11 +50,20 @@ public class LibraryEntity {
 	/**
 	 * 放弃关联维护 - 参照对方的关系进行维护
 	 * JsonIgnore ： json 在序列化的时候 忽略bookEntities属性
+	 * @OneToMany(targetEntity = BookEntity.class)
+	 * @JoinColumn(name = "assid", referencedColumnName = "lid", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
 	 */
-//	@OneToMany(targetEntity = BookEntity.class)
-//	@JoinColumn(name = "assid", referencedColumnName = "lid", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+
+
 	/**
-	 * cascade 级联操作 - CascadeType.ALL （相当于 ）
+	 *
+	 * 	@OneToMany : 一对多（声明一的一方）
+	 *		mappedBy ： 放弃关联键维护 （一对多关联关系参照对方）
+	 *			libraryEntity	: 这里指的是BookEntity中的绑定的 LibraryEntity 的名称 libraryEntity
+	 *		cascade	: 级联操作
+	 *			CascadeType.ALL ：主从的增删改查可以保持同步 （也可单独开启，一般全部开启）
+	 *	@JsonIgnore ： Json压缩时忽略此属性（此属性压缩时不做处理）当使用Springboot的@RestController注解时，一定要追加
+	 * 	List<BookEntity> bookEntities = new ArrayList<>(); 多的一方的实体类放入List中
 	 *
 	 */
 	@OneToMany(mappedBy = "libraryEntity", cascade = CascadeType.ALL)
